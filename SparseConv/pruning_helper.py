ebbeec874ae3f7e39ab4e0b818f96de050a47208
Nicola Dean <50876287+NicolaDean@users.noreply.github.com>
2023-03-17 14:27:33 +0100

    Added basic helper functions
    Copied some functions from original repo about sparse conv: https://github.com/VITA-Group/Structure-LTH

---
+++ SparseConv/pruning_helper.py
@@ -0,0 +1,57 @@
+from pruning import * #Chen group Methods
+import copy
+
+def applyPruningRegroup(model,initialization,pruning_rate):
+    '''
+    Apply chen regroup + IMP pruning:
+        1.Prune the model using standard L1 pruning
+        2.Extract an optimal mask from this pruning
+        3.Regroup the weights using the compact sparse format described in: paper
+        4.Remove pruning layer
+        5.Reset weights to initial values
+        6.Use the custom "regroup-friendly" pruning method.
+    '''
+    #Step1:
+    pruning_model(model, pruning_rate, conv1=False)
+    remain_weight = check_sparsity(model, conv1=False)
+    #Step2:
+    current_mask = extract_mask(model.state_dict())
+    current_mask_copy = copy.deepcopy(current_mask)
+    #Step3:
+    for m in current_mask_copy:
+        mask = current_mask_copy[m]
+        shape = mask.shape
+        current_mask[m] = regroup(mask.view(mask.shape[0], -1)).view(*shape)
+    #Step4:
+    remove_prune(model, conv1=False)
+    #Step5:
+    model.load_state_dict(initialization)
+    #Step6:
+    prune_model_custom(model, current_mask)
+
+    check_sparsity(model, conv1=False)
+
+    return
+
+def applyPruningRefill(model,initialization,pruning_rate):
+    '''
+    Apply chen refill + IMP pruning:
+        1.Prune the model using standard L1 pruning
+        2.Extract an optimal mask from this pruning
+        3.Remove pruning layer
+        4.Reset weights to initial values
+        5.Use the custom "regroup-friendly" pruning method.
+    '''
+    #Step1:
+    pruning_model(model, args.rate, conv1=False)
+    remain_weight = check_sparsity(model, conv1=False)
+    #Step2:
+    current_mask = extract_mask(model.state_dict())
+    for m in current_mask:
+        print(current_mask[m].float().mean())
+    #Step3:
+    remove_prune(model, conv1=False)
+
+    model.load_state_dict(initialization)
+    prune_model_custom_fillback(model, current_mask, train_loader=train_loader)
+    check_sparsity(model, conv1=False)
